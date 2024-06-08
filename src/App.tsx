b0fafab87d27c953c90348cc5062ac276e8557e5
lorenino <faloloren@yahoo.com>
2024-06-08 03:26:58 +0200

    [ADD] Log in / Cookie / restriction / role (simulé localment)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './screen/HomePage';
-import RegisterScreen from './screen/RegisterScreen';
 import LoginScreen from './screen/LoginScreen';
 import PriseRdvScreen from './screen/PriseRdvScreen';
 import PriseRdvMiParcoursScreen from './screen/PriseRdvMiParcoursScreen';
@@ -9,83 +8,149 @@ import RelancesScreen from './screen/RelancesScreen';
 import BilanScreen from './screen/BilanScreen';
 import GestionComptesScreen from './screen/GestionComptesScreen';
 import AlertesGeneralesScreen from './screen/AlertesGeneralesScreen';
-import SuiviEntretiensScreen from './screen/SuiviEntretiensScreen'; // Importer la nouvelle page
-
+import SuiviEntretiensScreen from './screen/SuiviEntretiensScreen';
 import logo from './assets/img/logoSU.png';
 import userImage from './assets/img/userPicture.png';
-import './App.css'; // Assurez-vous d'importer le fichier CSS global
+import './App.css';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+const Navbar: React.FC = () => {
+  const { user, logout } = useAuth();
+
   return (
-    <Router>
-      <div className="navbar">
-        <div className='navbar-title-container'>
-          <div>
-            <img src={logo} className='navbar-logo' alt='logo' />
-            <div className='separator'></div>
+    <div className="navbar">
+      <div className='navbar-title-container'>
+        <div>
+          <img src={logo} className='navbar-logo' alt='logo' />
+          <div className='separator'></div>
+          {user && (
             <div className='userImg-container'>
               <img className="user-img" src={userImage} alt="" />
-              <h3>Hugo Decrypte</h3>
+              <h3>{user.email}</h3>
+              <button onClick={logout}>Logout</button>
             </div>
-          </div>
+          )}
         </div>
-        <div className='separator'></div>
-        <div className='navbar-links'>
-          <Link to="/home" className='navbar-item'>
-            <div className='navbar-item-icon'><p>🏠</p></div>
-            <div className='navbar-item-title'><p>Home</p></div>
-          </Link>
-          <Link to="/register" className='navbar-item'>
-            <div className='navbar-item-icon'><p>📝</p></div>
-            <div className='navbar-item-title'><p>Register</p></div>
-          </Link>
+      </div>
+      <div className='separator'></div>
+      <div className='navbar-links'>
+        <Link to="/home" className='navbar-item'>
+          <div className='navbar-item-icon'><p>🏠</p></div>
+          <div className='navbar-item-title'><p>Home</p></div>
+        </Link>
+        {!user && (
           <Link to="/login" className='navbar-item'>
             <div className='navbar-item-icon'><p>🔑</p></div>
             <div className='navbar-item-title'><p>Login</p></div>
           </Link>
-          <Link to="/rdv" className='navbar-item'>
-            <div className='navbar-item-icon'><p>📅</p></div>
-            <div className='navbar-item-title'><p>Prise de RDV</p></div>
-          </Link>
-          <Link to="/rdv-mi-parcours" className='navbar-item'>
-            <div className='navbar-item-icon'><p>📅</p></div>
-            <div className='navbar-item-title'><p>RDV Mi-Parcours</p></div>
-          </Link>
-          <Link to="/relances" className='navbar-item'>
-            <div className='navbar-item-icon'><p>🔄</p></div>
-            <div className='navbar-item-title'><p>Relances</p></div>
-          </Link>
-          <Link to="/bilan" className='navbar-item'>
-            <div className='navbar-item-icon'><p>📊</p></div>
-            <div className='navbar-item-title'><p>Bilan</p></div>
-          </Link>
-          <Link to="/gestion-comptes" className='navbar-item'>
-            <div className='navbar-item-icon'><p>👥</p></div>
-            <div className='navbar-item-title'><p>Gestion Comptes</p></div>
-          </Link>
-          <Link to="/alertes" className='navbar-item'>
-            <div className='navbar-item-icon'><p>⚠️</p></div>
-            <div className='navbar-item-title'><p>Alertes Générales</p></div>
-          </Link>
-          <Link to="/suivi-entretiens" className='navbar-item'>
-            <div className='navbar-item-icon'><p>📋</p></div>
-            <div className='navbar-item-title'><p>Suivi des Entretiens</p></div>
-          </Link>
-        </div>
+        )}
+        {user && (
+          <>
+            {user.role === 'Admin / Directeur' && (
+              <Link to="/gestion-comptes" className='navbar-item'>
+                <div className='navbar-item-icon'><p>👥</p></div>
+                <div className='navbar-item-title'><p>Gestion Comptes</p></div>
+              </Link>
+            )}
+            <Link to="/rdv" className='navbar-item'>
+              <div className='navbar-item-icon'><p>📅</p></div>
+              <div className='navbar-item-title'><p>Prise de RDV</p></div>
+            </Link>
+            <Link to="/rdv-mi-parcours" className='navbar-item'>
+              <div className='navbar-item-icon'><p>📅</p></div>
+              <div className='navbar-item-title'><p>RDV Mi-Parcours</p></div>
+            </Link>
+            <Link to="/relances" className='navbar-item'>
+              <div className='navbar-item-icon'><p>🔄</p></div>
+              <div className='navbar-item-title'><p>Relances</p></div>
+            </Link>
+            <Link to="/bilan" className='navbar-item'>
+              <div className='navbar-item-icon'><p>📊</p></div>
+              <div className='navbar-item-title'><p>Bilan</p></div>
+            </Link>
+            <Link to="/alertes" className='navbar-item'>
+              <div className='navbar-item-icon'><p>⚠️</p></div>
+              <div className='navbar-item-title'><p>Alertes Générales</p></div>
+            </Link>
+            <Link to="/suivi-entretiens" className='navbar-item'>
+              <div className='navbar-item-icon'><p>📋</p></div>
+              <div className='navbar-item-title'><p>Suivi des Entretiens</p></div>
+            </Link>
+          </>
+        )}
       </div>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path='/register' element={<RegisterScreen />} />
-        <Route path='/login' element={<LoginScreen />} />
-        <Route path='/rdv' element={<PriseRdvScreen />} />
-        <Route path='/rdv-mi-parcours' element={<PriseRdvMiParcoursScreen />} />
-        <Route path='/relances' element={<RelancesScreen />} />
-        <Route path='/bilan' element={<BilanScreen />} />
-        <Route path='/gestion-comptes' element={<GestionComptesScreen />} />
-        <Route path='/alertes' element={<AlertesGeneralesScreen />} />
-        <Route path='/suivi-entretiens' element={<SuiviEntretiensScreen />} /> {/* Nouvelle page */}
-      </Routes>
-    </Router>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route
+            path='/rdv'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <PriseRdvScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/rdv-mi-parcours'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <PriseRdvMiParcoursScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/relances'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <RelancesScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/bilan'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <BilanScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/gestion-comptes'
+            element={
+              <ProtectedRoute roles={['Admin / Directeur']}>
+                <GestionComptesScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/alertes'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <AlertesGeneralesScreen />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/suivi-entretiens'
+            element={
+              <ProtectedRoute roles={['Suiveur', 'Admin / Directeur']}>
+                <SuiviEntretiensScreen />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,52 +1,114 @@
 import React, { useEffect, useState } from 'react';
 import "../App.css";
 import registerLogo from '../assets/img/logoSU.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const predefinedUsers = [
+    { id: 1, email: 'admin@example.com', password: 'admin123', role: 'Admin / Directeur' },
+    { id: 2, email: 'suiveur@example.com', password: 'suiveur123', role: 'Suiveur' },
+    { id: 3, email: 'tuteur@example.com', password: 'tuteur123', role: 'Tuteur' },
+    { id: 4, email: 'pedagogique@example.com', password: 'pedagogique123', role: 'Responsable pédagogique' },
+    { id: 5, email: 'cre@example.com', password: 'cre123', role: 'Responsable relations entreprises (Cre)' }
+];
 
 const LoginScreen: React.FC = () => {
-    const [companyCode, setCompanyCode] = React.useState<string>('');
     const [disableScroll, setDisableScroll] = useState(true);
-    
-    const [email, setEmail] = React.useState<string>('');
-    const [password, setPassword] = React.useState<string>('');
-    const [token, setToken] = React.useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
+    const navigate = useNavigate();
+    const { login } = useAuth();
 
     useEffect(() => {
-        if (disableScroll == true) {
+        if (disableScroll) {
             document.body.classList.add('disable-scroll');
+        } else {
+            document.body.classList.remove('disable-scroll');
         }
-        
+
         return () => {
             document.body.classList.remove('disable-scroll');
         };
     }, [disableScroll]);
 
+    const handleLogin = () => {
+        if (!email || !password) {
+            setError('Veuillez entrer votre email et mot de passe');
+            return;
+        }
 
+        const user = predefinedUsers.find(user => user.email === email && user.password === password);
+
+        if (!user) {
+            setError('Email ou mot de passe incorrect');
+            return;
+        }
+
+        login(user);
+        
+        // Navigate based on user role
+        switch (user.role) {
+            case 'Admin / Directeur':
+                navigate('/admin-dashboard');
+                break;
+            case 'Suiveur':
+                navigate('/suiveur-dashboard');
+                break;
+            case 'Tuteur':
+                navigate('/tuteur-dashboard');
+                break;
+            case 'Responsable pédagogique':
+                navigate('/pedagogique-dashboard');
+                break;
+            case 'Responsable relations entreprises (Cre)':
+                navigate('/cre-dashboard');
+                break;
+            default:
+                navigate('/user-dashboard');
+                break;
+        }
+    };
 
     return (
-        <div className='container-register' style={{paddingBottom:"100%"}} >
+        <div className='container-register'>
             <div className='Form'>
                 <div className='form-container'>
                     <div className='register-form'>
-                    <img src={registerLogo} className='register-img' alt="" />
-                    <h2 className=''>Se connecter</h2>
-                       <div className='register-item'>
-                            {/* <FontAwesomeIcon icon={faEnvelope} /> */}
-                            <input className='input-register' onChange={(e)=> setEmail(e.target.value)} type="text"  placeholder='Votre Mail'/>
-                       </div>
+                        <img src={registerLogo} className='register-img' alt="Logo" />
+                        <h2>Se connecter</h2>
 
-                       <div className='register-item'>
-                            {/* <FontAwesomeIcon icon={faUser} /> */}
-                            <input className='input-register' onChange={(e)=> setPassword(e.target.value)} type="text"  placeholder='Votre Mot de passe'/>
-                       </div>
+                        {error && <div className='error-message'>{error}</div>}
 
-                        <Link to='/register/validate'>
-                            <button className='register-button' onClick={() =>{} } type='button'>Se connecter</button>
-                        </Link>
+                        <div className='register-item'>
+                            <input
+                                className='input-register'
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="text"
+                                placeholder='Votre Mail'
+                                value={email}
+                            />
+                        </div>
 
+                        <div className='register-item'>
+                            <input
+                                className='input-register'
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                placeholder='Votre Mot de passe'
+                                value={password}
+                            />
+                        </div>
+
+                        <button className='register-button' onClick={handleLogin} type='button'>
+                            Se connecter
+                        </button>
+
+                        <div className='register-item'>
+                            <Link to='/forgot-password'>Mot de passe oublié?</Link>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
